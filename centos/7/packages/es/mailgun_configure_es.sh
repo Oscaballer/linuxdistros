48c29a066e35f71a48b19386826d5405b8f0cbcd
Oscaballer <sistemas@pixelhaiha.com>
2016-04-11 15:43:50 -0400

    fix centos7 mailgun fx2

--- centos/7/packages/es/mailgun_configure_es.sh
+++ centos/7/packages/es/mailgun_configure_es.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #https://gist.githubusercontent.com/jgeewax/2c63810a534197c8a6be/raw/203189bf2b8e6ed26bba227085ff40f5e8f4bfc2/configure-mailgun
 # Configuration for the script
-sudo su
+sudo su -
 
 POSTFIX_CONFIG=/etc/postfix/main.cf
 POSTFIX_SASL=/etc/postfix/sasl_passwd
 
 function confirm(){
   read -r -p "${1:-Estas segur@? [Y/n]} " response
-  if [[ $response == "" || $response == "y"  || $response == "Y" ]]; then
-    echo 0;
+  if [[ $response == "" || $response == "y"  || $response == "Y" ]]; 
+  then
+    echo 0
   else
-    echo 1;
+    echo 1
   fi
 }
 
 # Check that we're root, otherwise exit!
-if [ "$(id -u)" != "0" ]; then
+if [ "$(id -u)" != "0" ]; 
+then
    echo "Este script se debe ejecutar como root!" 1>&2
    exit 1
 fi
@@ -26,8 +28,9 @@ read -r -p "Escriba el Mailgun SMTP login: " SMTP_LOGIN
 read -r -p "Escriba el Mailgun SMTP password: " SMTP_PASSWORD
 
 CONTINUE=`confirm "Continuar con $SMTP_LOGIN:$SMTP_PASSWORD? [Y/n] "`
-if [[ CONTINUE -ne 0 ]]; then
-  echo "Cancelado...";
+if [[ CONTINUE -ne 0 ]]; 
+then
+  echo "Cancelado..."
   exit;
 fi
 
@@ -35,7 +38,7 @@ fi
 umask 077
 
 # Install postfix and friends
-apt-get update && apt-get install postfix libsasl2-modules -y
+yum install postfix cyrus-sasl-plain cyrus-sasl-md5 -y
 
 # Comment out a couple transport configurations
 sed -i.bak "s/default_transport = error/# default_transport = error/g" $POSTFIX_CONFIG
@@ -60,10 +63,12 @@ EOF
 
 # Generate a .db file and remove the old file
 postmap $POSTFIX_SASL
+ls -l $POSTFIX_SASL*
 rm $POSTFIX_SASL
 
 # Set the permissions on the .db file
 chmod 600 $POSTFIX_SASL.db
+ls -la /etc/postfix/sasl_passwd.db
 
 # Reload Postfix
-/etc/init.d/postfix restart
+postfix reload
